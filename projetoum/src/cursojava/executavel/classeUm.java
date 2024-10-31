d7111462c9bcad3a03b0e3de22fd34018af1a281
wellintons <wellintons@DPGE-20032.dpge.MS>
2024-10-31 15:32:09 -0400

    criação do projeto

---
+++ projetoum/src/cursojava/executavel/classeUm.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class classeUm {
+	
+	/*main é um método auto executável em java*/
+	public static void main(String[] args) {
+		System.out.println("texto a ser exibido. alterado");
+		System.out.println("métodos e funções");
+	}
+	
+}
